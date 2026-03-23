c072cbec21378d9b3721ae574e98ae5c292655ae
jeremy <gamecoding@bluemailx.com>
2026-03-23 21:36:52 +0100

    Update RealmCore.sol

--- RealmCore.sol
+++ RealmCore.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RealmCore is ERC20 {
+    constructor() ERC20("Realm Core", "REALM") {
+        uint256 initialSupply = 150_000_000 * 10 ** decimals();
+        _mint(msg.sender, initialSupply);
+    }
+}
